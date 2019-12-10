3d16f863e34b13a7eaaf99395e55bd0a8f4c1a98
gebeer <gerhard.sitzmann@gmail.com>
2019-12-10 21:03:02 +0700

    change loading spinner to uk-spinner

--- InputfieldImageFromPage.js
+++ InputfieldImageFromPage.js
@@ -1,7 +1,5 @@
 var InputfieldImageFromPage = {
     init: function () {
-        // console.log('init');
-        InputfieldImageFromPage.loader = "<svg width=\"38\" height=\"38\" xmlns=\"http://www.w3.org/2000/svg\" stroke=\"#000\"><g transform=\"translate(1 1)\" stroke-width=\"2\" fill=\"none\" fill-rule=\"evenodd\"><circle stroke-opacity=\".5\" cx=\"18\" cy=\"18\" r=\"18\"/><path d=\"M36 18c0-9.94-8.06-18-18-18\"><animateTransform attributeName=\"transform\" type=\"rotate\" from=\"0 18 18\" to=\"360 18 18\" dur=\"1s\" repeatCount=\"indefinite\"/></path></g></svg>";
         var fields = document.querySelectorAll('.InputfieldImageFromPage');
         fields.forEach(field => {
             if (field.classList.contains('imagefrompage_initialised')) return;
@@ -56,7 +54,7 @@ var InputfieldImageFromPage = {
         });
     },
     fetchAndInsertThumbnails: function(url, thumbnav, field) {
-        thumbnav.innerHTML = InputfieldImageFromPage.loader;
+        thumbnav.innerHTML = '<div uk-spinner></div>';
         fetch(url)
         .then(function (response) {
             if (!response.ok) {

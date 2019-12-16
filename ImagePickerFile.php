46f021e6f5eadbbb4d8f463489c09ad4f0bb8c8d
gebeer <gerhard.sitzmann@gmail.com>
2019-12-16 08:43:55 +0700

    added

---
+++ ImagePickerFile.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ProcessWire;
+
+/**
+ * Class ImagePickerFile 
+ * 
+ * Holds information about an image file
+ */
+
+class ImagePickerFile extends WireData
+{
+    public function __construct() {
+        parent::__construct();
+    }
+
+}

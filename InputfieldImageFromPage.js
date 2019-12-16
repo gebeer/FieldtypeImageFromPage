48aabb6c3a7669b5ddc45d327515360527ba80d4
gebeer <gerhard.sitzmann@gmail.com>
2019-12-16 08:44:38 +0700

    changed to delegated events

--- InputfieldImageFromPage.js
+++ InputfieldImageFromPage.js
@@ -21,63 +21,60 @@ var InputfieldImageFromPage = {
             caption.innerHTML = '';
             inputValue.value = '';
         });
-        files.forEach(file => {
-            file.addEventListener('click', function () {
-                var src = file.getAttribute('src');
-                var fileinfo = file.getAttribute('uk-tooltip');
-                var filename = file.getAttribute('data-filename');
-                var pageid = file.getAttribute('data-pageid');
-                preview.setAttribute('src', src);
-                inputValue.value = JSON.stringify({"pageid": pageid, "filename": filename});
-                caption.innerHTML = fileinfo;
-            })
+        $(field).on('click', '.uk-thumbnav img', function (e) {
+            var file = this;
+            var src = file.getAttribute('src');
+            var fileinfo = file.getAttribute('uk-tooltip');
+            var filename = file.getAttribute('data-filename');
+            var pageid = file.getAttribute('data-pageid');
+            preview.setAttribute('src', src);
+            inputValue.value = JSON.stringify({ "pageid": pageid, "filename": filename });
+            caption.innerHTML = fileinfo;
         });
-
     },
     initGetThumbnails: function (field) {
-        var thumbHolders = field.querySelectorAll('.imagefrompage_thumbholder');
-
-        thumbHolders.forEach(th => {
-            var thumbnav = th.querySelector('.uk-thumbnav');
+        $(field).on('click', '.imagefrompage_thumbholder', function (e) {
+            if (e.target.closest('.uk-thumbnav')) return;
+            var target = e.currentTarget;
+            var thumbnav = target.querySelector('.uk-thumbnav');
             var pageid = thumbnav.getAttribute('data-pageid');
-            th.addEventListener('click', function (e) {
-                var url = ProcessWire.config.InputfieldImageFromPage.url + '&pageid=' + pageid;
-                var closed = th.classList.contains('InputfieldStateCollapsed');
-                var empty = thumbnav.querySelector('li') === null;
-                if (closed && empty) {
-                    InputfieldImageFromPage.fetchAndInsertThumbnails(url, thumbnav, field);
-                }
-            })
+            var url = ProcessWire.config.InputfieldImageFromPage.url + '&pageid=' + pageid;
+            var closed = target.classList.contains('InputfieldStateCollapsed');
+            var empty = thumbnav.querySelector('li') === null;
+            if (closed && empty) {
+                InputfieldImageFromPage.fetchAndInsertThumbnails(url, thumbnav);
+            }
         });
+
     },
-    fetchAndInsertThumbnails: function(url, thumbnav, field) {
+    fetchAndInsertThumbnails: function (url, thumbnav) {
         thumbnav.innerHTML = '<div uk-spinner></div>';
         fetch(url)
-        .then(function (response) {
-            if (!response.ok) {
-                throw Error(response.statusText);
-            }
-            return response.text();
-        })
-        .then(function (html) {
-            thumbnav.innerHTML = html;
-            InputfieldImageFromPage.initPickFile(field);
-        })
-        .catch(function (error) {
-            console.log('Looks like there was a problem: \n', error);
-        });
-    },
-    initModalEditImages: function() {
-        $(document).on("pw-modal-closed", function(event, ui) {
-                if($(event.target).hasClass('imagefrompage_editimages')) {
-                    var link = $(event.target);
-                    var field = link.closest('.InputfieldImageFromPage')[0];
-                    var thumbnav = link.siblings('.uk-thumbnav')[0];
-                    var pageid = thumbnav.getAttribute('data-pageid');
-                    var url = ProcessWire.config.InputfieldImageFromPage.url + '&pageid=' + pageid;
-                    InputfieldImageFromPage.fetchAndInsertThumbnails(url, thumbnav, field);
+            .then(function (response) {
+                if (!response.ok) {
+                    throw Error(response.statusText);
                 }
-        })        
+                return response.text();
+            })
+            .then(function (html) {
+                thumbnav.innerHTML = html;
+                // InputfieldImageFromPage.initPickFile(field);
+            })
+            .catch(function (error) {
+                console.log('Looks like there was a problem: \n', error);
+            });
+    },
+    initModalEditImages: function () {
+        $(document).on("pw-modal-closed", function (event, ui) {
+            if ($(event.target).hasClass('imagefrompage_editimages')) {
+                var link = $(event.target);
+                var field = link.closest('.InputfieldImageFromPage')[0];
+                var thumbnav = link.siblings('.uk-thumbnav')[0];
+                var pageid = thumbnav.getAttribute('data-pageid');
+                var url = ProcessWire.config.InputfieldImageFromPage.url + '&pageid=' + pageid;
+                InputfieldImageFromPage.fetchAndInsertThumbnails(url, thumbnav, field);
+            }
+        })
     }
 }
 
